c39f0b95ec4cabe5989fab503a9cb9cb9b88738d
paulscrugham <paulscrugham@gmail.com>
2022-05-30 15:19:55 -0700

    Read user data from file and consolidate classes

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from turtle import color
-from pkg_resources import EntryPoint
-from gps import GPS
-from sat import SAT
+from pisat import PiSat
 from skyfield.api import load
 import PySimpleGUI as sg
 import math
+import json
 
 DEBUG = False
 
@@ -12,58 +10,27 @@ REFRESH_RATE = 1000  # used with window.read() to set the data refresh rate
 
 # constant variable to store user provided sat data.
 # format: {ID:[UP, DOWN]}
-USER_SATS = {
-	'25544': 	{'up': '437.550', 			'down': '437.550'}, # ISS
-	'7530': 	{'up': '145.850-145.950', 	'down':'29.400- 29.500'}, # OSCAR 7
-	'14781': 	{'up': '', 					'down': '145.826/435.025'}, # OSCAR 11
-	'22825': 	{'up': '145.850', 			'down': '436.795 '}, # EYESAT-1
-	'24278': 	{'up': '145.900-146.000', 	'down': '435.900-435.800'}, # JAS 2 (FO-29)
-	'27844': 	{'up': '', 					'down': '437.470 '}, # CUTE-1
-	'23439': 	{'up': '', 					'down': ''},
-	'26931': 	{'up': '', 					'down': ''},
-	'20442': 	{'up': '', 					'down': ''},
-	'25397': 	{'up': '', 					'down': ''},
-	'36122': 	{'up': '', 					'down': ''},
-	'37839': 	{'up': '', 					'down': ''},
-	'37841': 	{'up': '', 					'down': ''},
-	'37855': 	{'up': '', 					'down': ''},
-	'39090': 	{'up': '', 					'down': ''},
-	'39134': 	{'up': '', 					'down': ''},
-	'39135': 	{'up': '', 					'down': ''},
-	'39136': 	{'up': '', 					'down': ''},
-	'39417': 	{'up': '', 					'down': ''},
-	'39430': 	{'up': '', 					'down': ''},
-	'39440': 	{'up': '', 					'down': ''},
-	'39444': 	{'up': '', 					'down': ''},
-	'39446': 	{'up': '', 					'down': ''},
-	'39770': 	{'up': '', 					'down': ''},
-	'40012': 	{'up': '', 					'down': ''},
-	'40021': 	{'up': '', 					'down': ''},
-	'40025': 	{'up': '', 					'down': ''},
-	'40900': 	{'up': '', 					'down': ''},
-	'40901': 	{'up': '', 					'down': ''},
-	'40902': 	{'up': '', 					'down': ''},
-	'40903': 	{'up': '', 					'down': ''}
-	
-}
 
-# initialize satellites object
-satellites = SAT(USER_SATS)
-satellites.load_tles()
-if DEBUG: print(satellites)
+sat_file = open('user_sats.json')
+USER_SATS = json.load(sat_file)
 
-# initialize GPS object for observer
-# TODO: add while loop to handle sat lock - took awhile to start streaming GPS data on 5/12
-observer = GPS()
-pos = observer.update_pos()
-if DEBUG: print("Observer position:", observer.get_pos())
+# initialize PiSat
+ps = PiSat(USER_SATS, expiry=5)
+ps.load_tles('https://celestrak.com/NORAD/elements/gp.php?GROUP=amateur&FORMAT=tle') # downloads TLEs from CelesTrak from the amsat group
 
-events = satellites.calc_events(pos)
+# get current location of observer
+ps.update_pos()
+if DEBUG: print("Observer position:", ps.get_pos())
+
+events = ps.calc_events()
 print(events[25544][0])
 print(events[25544][1])
 
+# ps.calc_ephem()
+
 for ti, event in zip(events[25544][0], events[25544][1]):
 	name = ('rise above 0°', 'culminate', 'set below 0°')[event]
+	print(type(ti))
 	print(ti.utc_strftime('%Y %b %d %H:%M:%S'), name)
 
 sg.theme('Dark')
@@ -75,7 +42,7 @@ headings = ['ID', 'NAME', 'ALTITUDE', 'AZIMUTH', 'DISTANCE', 'UPLINK', 'DOWNLINK
 # TODO: change list of headings to look up table
 for row, entry in enumerate(USER_SATS):
 	sat_data.append([])
-	sat = satellites.get_tle(entry)
+	sat = ps.get_tle(entry)
 	for col in headings:
 		if col == 'ID':
 			sat_data[row].append(sat.model.satnum)
@@ -152,7 +119,6 @@ tab2_layout = [[sg.Graph(canvas_size=(C_LEN, C_LEN),
 tab_group_layout = [[
 	sg.Tab('Table', tab1_layout, font='Courier 15', key='-TAB1-'),
 	sg.Tab('Skyplot', tab2_layout, font='Courier 15', key='-TAB2-')
-
 ]]
 
 layout = [[sg.TabGroup(tab_group_layout,
@@ -175,31 +141,37 @@ sat_points = []
 sat_labels = []
 sat_paths = []
 for entry in USER_SATS:
-	sat = satellites.get_tle(entry)
+	sat = ps.get_tle(entry)
 	sat_points.append(skyplot.draw_point(C_CENTER, size=6, color='red'))
 	sat_labels.append(skyplot.draw_text(sat.name, (C_CENTER[0], C_CENTER[1]), color='white'))
 	sat_paths.append(None)
 
+# main application loop
 while True:
 	event, values = window.read(REFRESH_RATE)
 	if DEBUG: print(event, values)
 
+	# loop to update satellite date
 	for row, entry in enumerate(USER_SATS):
-		sat = satellites.get_tle(entry)  # look up sat TLE in dictionary of all sats
-		alt, az, distance = observer.calc_diff(sat)
+		sat = ps.get_tle(entry)  # look up sat TLE in dictionary of all sats
+		alt, az, distance = ps.calc_diff(sat)
+		
+		# TABLE: save instantaneous satellite position data for table
 		sat_data[row][2] = round(alt.degrees, 2)
 		sat_data[row][3] = round(az.degrees, 2)
 		sat_data[row][4] = round(distance.km, 2)
+
+		# SKYPLOT: move satellite dot for skyplot view
 		new_x, new_y = plot_polar(az.degrees, alt.degrees)
 		skyplot.RelocateFigure(sat_points[row], new_x, new_y)
 		skyplot.RelocateFigure(sat_labels[row], new_x, new_y + 10)
 
-		# draw flight path if satellite is above 0 degrees
+		# SKYPLOT: draw flight path if satellite is above 0 degrees
 		if alt.degrees >= 0 and sat_paths[row] is None:
 			# reset ephemeris for satellite to None
 			sat_paths[row] = []
 			# generate ephemeris (alt, az) for satellite
-			path = observer.calc_path(sat)
+			path = ps.calc_path(sat)
 			# convert az and alt values to screen coordinates
 			for i in range(len(path)- 1):
 				point_from = plot_polar(path[i][0], path[i][1])
@@ -222,8 +194,5 @@ while True:
 	window['SAT_TABLE'].update(sat_data)
 
 
-
-
 window.close()
-
-	
+sat_file.close()
